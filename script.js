0e29fc51e6155385d86755b1ff10e7a08f20fa8c
minmeeen <sirapat.mp@gmail.com>
2021-08-05 14:27:29 +0700

    edit

--- script.js
+++ script.js
@@ -3,11 +3,38 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
+const btn_search  = document.getElementById('search')
+const btn_reset = document.getElementById('reset')
+const text = document.getElementById('text');
 
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  // your code here
+  const chName = document.getElementById("author");
+  if (chName.innerText === "630610768") {
+    chName.innerText = "630610768 Sirapat Panyathong";
+  } else {
+    chName.innerText = "630610768";
+  }
 }
 
-// more codes for Search and Reset buttons here
+
+btn_search.onclick = () => {
+  // let num = length.value;
+  // let para = document.getElementById('text');
+  document.getElementById('text').innerHTML = '';
+
+
+
+
+}
+
+btn_reset.onclick = () => {
+  // const newText = document.createElement("p");
+  // newText.innerText = ("text");
+  document.getElementById("text").reset();
+}
+
+// const color = (ev) => {
+// 	document.querySelector('body').style.backgroundColor = ev.target.value	
+// }
